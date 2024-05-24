df71ce0175c6b0276e22781bbfb6c40a8fc4e8f2
Andrew <92193378+Andrew7-7@users.noreply.github.com>
2024-05-24 20:47:30 +0700

    feat: memperbagus

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,86 @@
-
-
+import { useState } from "react";
 import "./App.css";
-
+import profileImage from "./assets/profile-icon-design-free-vector.jpg";
 function App() {
+  const [showMovie, setShowMovie] = useState(false);
+  const AboutUsCard = ({ name, image }: { name: string; image: string }) => {
+    return (
+      <div className="aboutUsDiv">
+        <img className="aboutUsImage" src={image} />
+        <p className="aboutUsName">{name}</p>
+      </div>
+    );
+  };
   return (
     <>
+      {showMovie ? (
+        <div className="movieOuter">
+          <div className="movieDiv">
+            <div className="closeDiv">
+              <p
+                onClick={() => setShowMovie(false)}
+                style={{ cursor: "pointer" }}
+              >
+                X
+              </p>
+            </div>
+            <p>Movie</p>
+            <video width="600" controls>
+              {/* <source
+            src={`${process.env.PUBLIC_URL}/sample.mp4`}
+            type="video/mp4"
+          /> */}
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+      ) : null}
       <div className="header">
         Welcome to our Short Movie
         <p className="madeBy">Made by Kelompok 5</p>
       </div>
 
-      <p className="title">PINJOL</p>
-
       <div className="moviePlace">
-        <video width="600" controls>
-          {/* <source
+        <div className="trailerDiv">
+          <p className="title">PINJ*L Trailer</p>
+          <video width="600" controls>
+            {/* <source
             src={`${process.env.PUBLIC_URL}/sample.mp4`}
             type="video/mp4"
           /> */}
-          Your browser does not support the video tag.
-        </video>
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="trailerDiv">
+          <p className="title">Sinopsis</p>
+          <div className="sinopsisDetail">Sinopsis Detail</div>
+        </div>
+      </div>
+
+      <div
+        className="clickMovie"
+        onClick={() => {
+          setShowMovie(true);
+        }}
+      >
+        Click Here to watch the movie
       </div>
 
       <div className="aboutUs">
-        <p>Andrew</p>
-        <p>Eugene Emmanuelle Hervine</p>
-        <p>Arya Yudhistira Ramadhan</p>
-        <p>Pierre Sitorus</p>
-        <p>Alvrian Timothius</p>
-        <p>Christopher Jovison</p>
+        <p className="aboutUsTitle">About Us</p>
+        <div className="bottomDiv">
+          <AboutUsCard name={"Andrew"} image={profileImage} />
+          <AboutUsCard
+            name={"Eugene Emmanuelle Hervine"}
+            image={profileImage}
+          />
+          <AboutUsCard name={"Arya Yudhistira Ramadhan"} image={profileImage} />
+        </div>
+        <div className="bottomDiv">
+          <AboutUsCard name={"Pierre Sitorus"} image={profileImage} />
+          <AboutUsCard name={"Alvrian Timothius"} image={profileImage} />
+          <AboutUsCard name={"Christopher Jovison"} image={profileImage} />
+        </div>
       </div>
     </>
   );
